8da6c1065dad70b884fa76a94090ab15b3fa263a
harshdev2909 <harsh.dev2909@gmail.com>
2024-12-19 11:18:52 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const cors = require('cors');
+app.use(cors());
+
+const port = process.env.PORT || 5000;
 
 app.use("/",(req,res)=>{
     res.send("Hello World");
 })
 
-app.listen(4000,(req,res)=>{
-    console.log('server is running');
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
 })
